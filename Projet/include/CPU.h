f7128d4b0679660c51ca67bedfba3b7cad0749dd
peltho <boitutile@gmail.com>
2012-12-22 15:24:11 +0000

    Avancement des questions 3*

--- Projet/include/CPU.h
+++ Projet/include/CPU.h
@@ -1,6 +1,6 @@
 #include "Board.h"
 
-#define MEMORYSIZEPERPROCESS     1000 
+#define MEMORYSIZEPERPROCESS     1000
 #define KERNELSAVECURRENTPROCID     0
 #define KERNELPID                   0 
 #define PROCPSWFRAMESTART           0 
@@ -9,7 +9,7 @@
 #define MAXSPROCCOUNT               9  // 9 processus max
 #define MAXSEMAPHORECOUNT           9  // 9 sémaphores max
 #define SEMAPHOREVECTORSTART      100
-#define PROCSEMWAITLISTSTART      200 // ProcsemWaitList semops (à partir de 200, cb de semops (couple id sema (décalage par rapport à 100), op)
+#define PROCSEMWAITLISTSTART      200  // ProcSemWaitList semops (à partir de 200, cb de semops (couple id sema (décalage par rapport à 100), op)
 #define PROCSEMWAITLISTSTARTELEMCNT 2
 #define CONSOLEINPUTOUTPUTTRIGGER 300
 
@@ -17,7 +17,7 @@ class CPU : public BaseCPU {
 public:
     CPU(const string &id, ostream &log, istream &consoleIn, ostream &consoleOut, const int nGenReg, const int nProc);
     ~CPU();
-    void execute  (const Instruction &instr) throw(nsSysteme::CExc); 
+    void execute  (const Instruction &instr) throw(nsSysteme::CExc);
     void interrupt(const int interruptNumber, bool qCLINTInstr) throw(nsSysteme::CExc);
     void pendingIntIfAny() throw(nsSysteme::CExc);
 };
